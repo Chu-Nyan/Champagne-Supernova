474f2c95c01192aa82c4da1c79e201b63428d25e
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2024-06-09 13:11:23 +0900

    병합 정렬 코드 추가

---
+++ ChampagneSupernova/Library/Sort/MergeSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Library.Sort
+{
+    public static class MergeSort<T> where T : IComparable<T>
+    {
+        private static T[] _tempArr;
+
+        public static void Start(T[] arr)
+        {
+            int length = arr.Length;
+            _tempArr = new T[length];
+            Split(arr, 0, length - 1);
+        }
+
+        private static void Split(T[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (right + left) / 2;
+
+                Split(arr, left, mid);
+                Split(arr, mid + 1, right);
+                Merge(arr, left, mid + 1, right);
+            }
+        }
+
+        private static void Merge(T[] arr, int left, int mid, int right)
+        {
+            int l = left;
+            int m = mid;
+            int r = right;
+            int index = l;
+
+            while (index <= r)
+            {
+                if (m > right || l < mid
+                 && arr[l].CompareTo(arr[m]) <= 0)
+                    _tempArr[index++] = arr[l++];
+                else
+                    _tempArr[index++] = arr[m++];
+            }
+
+            for (int i = left; i <= right; i++)
+            {
+                arr[i] = _tempArr[i];
+            }
+        }
+    }
+}
