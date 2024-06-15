082a71892a0e714599f21ebf81cdfaa50553ee63
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2024-06-15 22:12:56 +0900

    오브젝트 풀링 추가
    - 풀링을 해야하는 클래스에 IPoolable을 상속받고, 풀링을 실행할 클래스에 추가해주면 됨
    - Poolable : 굳이 사용 안 해도 됨

---
+++ ChampagneSupernova/Library/Design-Pattern/ObjectPooling.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.DesignPattern
+{
+    public class ObjectPooling<T> where T : class, IPoolable
+    {
+        private readonly Queue<T> _queue;
+        private readonly Func<T> _onNewGenerated;
+
+        public ObjectPooling(Func<T> generate)
+        {
+            _queue = new Queue<T>();
+            _onNewGenerated = generate;
+        }
+
+        public T GetNew()
+        {
+            if (_queue.TryDequeue(out var item) == false)
+                item = _onNewGenerated();
+            else
+                item.Init();
+
+            return item;
+        }
+    }
+
+    public interface IPoolable 
+    {
+        public void Init();
+    }
+}
