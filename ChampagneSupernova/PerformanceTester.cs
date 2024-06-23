df41213c494cd080f0a89604b312ce4917561e36
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2024-06-23 21:57:49 +0900

    feat: 함수 성능 테스터 추가

---
+++ ChampagneSupernova/PerformanceTester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ChampagneSupernova
+{
+    public static class PerformanceTester
+    {
+        private const int DefaultLoop = 10000;
+        private const int AvgLoop = 100;
+
+        public static void Start(Action method, int loop = DefaultLoop)
+        {
+            // TO DO : 절사 평균 값도 추가할 것
+
+            long allTick = 0;
+            for (int i = 0; i < AvgLoop; i++)
+            {
+                long loopTick = Test(method, loop);
+                allTick += loopTick;
+            }
+            TimeSpan span = new(allTick / AvgLoop);
+            long avgMs = span.Microseconds;
+
+            Console.WriteLine($"Count : {loop} | Avg : {avgMs}ms");
+        }
+
+        private static long Test(Action method, int loop)
+        {
+            long startTick = DateTime.UtcNow.Ticks;
+            for (int i = 0; i < loop; i++)
+            {
+                method.Invoke();
+            }
+            long endTick = DateTime.UtcNow.Ticks;
+
+            return endTick - startTick;
+        }
+    }
+}
