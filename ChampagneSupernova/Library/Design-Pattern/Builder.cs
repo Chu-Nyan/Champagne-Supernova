89e4640531cb8d544c493a40835df18569426558
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2024-06-29 09:27:01 +0900

    feat: 빌더 패턴 예제

---
+++ ChampagneSupernova/Library/Design-Pattern/Builder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Library.DesignPattern
+{
+    public class Builder : Singleton<Builder>
+    {
+        private readonly ObjectPooling<Stopwatch> _pooling;
+        private Stopwatch _new;
+
+        private event Action<Stopwatch> Generated;
+        private event Action<Stopwatch> Destroyed;
+
+        public Builder()
+        {
+            Func<Stopwatch> generate = () => { return new(); };
+            Action<Stopwatch> reset = (item) => { item.Reset(); };
+            _pooling = new(generate, reset);
+        }
+
+        public void Ready()
+        {
+            _new = _pooling.Dequeue();
+        }
+
+        public Builder SetName(string name)
+        {
+            return this;
+        }
+
+        public Stopwatch Generate()
+        {
+            Generated?.Invoke(_new);
+            return _new;
+        }
+
+        public void Clean(Stopwatch human)
+        {
+            Destroyed?.Invoke(human);
+        }
+    }
+}
