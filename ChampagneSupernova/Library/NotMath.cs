50ad59f3bec77f04f9278bf8fccbffebdede6de0
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2024-10-10 10:37:37 +0900

    feat: 수학 연산 클래스 개업식
    GCD, LCM을 곁들인...

---
+++ ChampagneSupernova/Library/NotMath.cs
@@ -0,0 +1,22 @@
+﻿namespace Library
+{
+    public static class NotMath
+    {
+        public static int GCDWithEuclidean(int n0, int n1)
+        {
+            while (n0 % n1 != 0)
+            {
+                var temp = n0;
+                n0 = n1;
+                n1 = temp % n1;
+            }
+
+            return n1;
+        }
+
+        public static int LCM(int n0, int n1)
+        {
+            return n0 * n1 / GCDWithEuclidean(n0, n1);
+        }
+    }
+}
