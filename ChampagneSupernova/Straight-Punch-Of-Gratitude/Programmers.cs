d535a018c1b9eb007ead7f8bc5042311bb56d54c
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-04-16 22:26:57 +0900

    감사의 정권 지르기!!

--- ChampagneSupernova/Straight-Punch-Of-Gratitude/Programmers.cs
+++ ChampagneSupernova/Straight-Punch-Of-Gratitude/Programmers.cs
@@ -5,6 +5,26 @@ using System.Text;
 
 namespace Programmers
 {
+    public class Day250416
+    {
+        public class Solution12914
+        {
+            public long solution(int n)
+            {
+                var dp = new int[2001];
+                dp[1] = 1;
+                dp[2] = 2;
+
+                for (int i = 3; i <= n; i++)
+                {
+                    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+                }
+
+                return dp[n];
+            }
+        }
+    }
+
     public class Day250415
     {
         public class Solution138476
